cd808158678a3a0e98098024c975f13bab2a36b8
Damian Enriquez <dcamarillo@eastridge.com>
2019-01-25 17:45:40 -0800

    updates

--- app/index.js
+++ app/index.js
@@ -3,3 +3,20 @@ import Icons from 'uikit/dist/js/uikit-icons';
 
 // loads the Icon plugin
 UIkit.use(Icons);
+
+
+UIkit.util.on('#offcanvas-overlay', 'show', function() {
+  document.getElementById('nav-menu-open').classList.add('uk-hidden');
+  document.getElementById('nav-menu-close').classList.remove('uk-hidden');
+});
+
+UIkit.util.on('#offcanvas-overlay', 'hide', function () {
+  document.getElementById('nav-menu-close').classList.add('uk-hidden');
+  document.getElementById('nav-menu-open').classList.remove('uk-hidden');
+});
+
+UIkit.util.on('#nav-menu-close', 'click', function(e) {
+  e.stopPropagation();
+  e.preventDefault();
+  UIkit.offcanvas('#offcanvas-overlay').hide();
+});
