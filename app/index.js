8091cb54a6478823e4d2eaa428e38d8b54d13563
Damian Enriquez <denriquez@tvapge.com>
2019-02-03 20:38:25 -0800

    updates

--- app/index.js
+++ app/index.js
@@ -1,13 +1,21 @@
-// var tumblr = require('tumblr.js');
 import UIkit from 'uikit';
 import Icons from 'uikit/dist/js/uikit-icons';
 import moment from 'moment';
+
+var $ = require("jquery");
 
 moment.locale('es')
 var isMenuOpen = false;
 
 // loads the Icon plugin
 UIkit.use(Icons);
+
+setTimeout(function () {
+  var pl = document.getElementById('page-loader');
+  document.body.removeChild(pl);
+  document.body.classList.remove("no-scroll");
+  document.getElementById('site-content').classList.remove('uk-invisible');
+}, 3000);
 
 UIkit.util.on('.uk-navbar-toggle', 'click', function (e) {
   if (isMenuOpen) {
@@ -16,6 +24,23 @@ UIkit.util.on('.uk-navbar-toggle', 'click', function (e) {
     openMenu();
   }
 });
+
+
+$(document).ready(function () {
+  var navBar = $('#left-nav');
+  var startchange = $('#content');
+  var offset = startchange.offset();
+  if (startchange.length) {
+    $(document).scroll(function () {
+      if (navBar.offset().top >= offset.top) {
+        navBar.addClass('on-content');
+      } else {
+        navBar.removeClass('on-content');
+      }
+    });
+  }
+});
+
 
 addEvent(document, "keydown", function (evt) {
   evt = evt || window.event;
@@ -48,6 +73,7 @@ function removeClasesToClass(className, removeClass) {
 
 function openMenu() {
   document.body.classList.add("is-menu-open");
+  document.body.classList.add("no-scroll");
   addClasesToClass('nav-menu-open', 'uk-hidden');
   removeClasesToClass('nav-menu-close', 'uk-hidden');
   isMenuOpen = true;
@@ -55,6 +81,7 @@ function openMenu() {
 
 function closeMenu() {
   document.body.classList.remove("is-menu-open");
+  document.body.classList.remove("no-scroll");
   addClasesToClass('nav-menu-close', 'uk-hidden');
   removeClasesToClass('nav-menu-open', 'uk-hidden');
   isMenuOpen = false;
@@ -82,13 +109,13 @@ var client = tumblr.createClient({
 
 client.blogPosts('esmikegarcia', {
   limit: '6'
-}, function(err, resp) {
-  resp.posts.forEach(function(post, index) {
+}, function (err, resp) {
+  resp.posts.forEach(function (post, index) {
     var tempDiv = document.createElement('div');
     tempDiv.innerHTML = post.body;
     var title = post.title || tempDiv.getElementsByTagName('h1')[0].innerHTML;
-    var image = tempDiv.getElementsByTagName('img').length > 0? tempDiv.getElementsByTagName('img')[0].getAttribute('src') : '';
-    var body =  tempDiv.getElementsByTagName('p').length > 0? tempDiv.getElementsByTagName('p') : [];
+    var image = tempDiv.getElementsByTagName('img').length > 0 ? tempDiv.getElementsByTagName('img')[0].getAttribute('src') : '';
+    var body = tempDiv.getElementsByTagName('p').length > 0 ? tempDiv.getElementsByTagName('p') : [];
     var bodyText = '';
 
     for (var i = 0; i < body.length; i++) {
@@ -103,5 +130,32 @@ client.blogPosts('esmikegarcia', {
     document.getElementById('news-link-' + index).setAttribute('href', link);
     document.getElementById('news-background-' + index).style.backgroundImage = 'url(' + image + ')';
   });
-  
+
 });
+
+var ig = require('instagram-node').instagram();
+// ig.use({ access_token: '26022854.ba4c844.9ab19fb4aa4342668c5d7301fe6f8dab' });
+ig.use({
+  client_id: 'e8ce2d2784bd4e478510f0ec9bOadc3b',
+  client_secret: '69e3ecb0d396406a8f76f545b5fce1e9',
+  access_token: '2074990290.e8ce2d2.1fa273b858d74d928eec5af57ef629c2'
+});
+
+ig.user_media_recent(
+  '2074990290',
+  {
+    count: 6
+  },
+  function (err, medias, pagination, remaining, limit) {
+    medias.forEach(function (media, index) {
+      document.getElementById('instagram-background-' + index).style.backgroundImage = 'url(' + media.images.standard_resolution.url + ')';
+      document.getElementById('instagram-url-' + index).setAttribute('href', media.link);
+    });
+  }
+);
+
+
+// ig.user('2074990290', function(err, result, remaining, limit) {});
+
+
+
